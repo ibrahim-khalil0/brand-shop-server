512ad907dc43277b389c18c9dff51ab4782e46d1
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2023-10-20 17:09:40 +0600

    cart product add in database and get from mongodb

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ async function run() {
     await client.connect();
 
     const productsCollection = client.db('productsDB').collection('products')
+    const cartCollection = client.db('productsDB').collection('cart')
 
 
     app.get('/products/:brand_name', async (req, res) => {
         const brand = req.params.brand_name;
-        console.log(brand);
     
         try {
             const cursor = productsCollection.find({ brand: brand }); // Filter data based on the brand name
@@ -53,16 +53,64 @@ async function run() {
       const id = req.params.id
       const query = {_id: new ObjectId(id)}
       const product = await productsCollection.findOne(query)
-      console.log(product)
       res.send(product)
     })
 
     app.post('/products', async(req, res) => {
         const newProduct = req.body
-        console.log(newProduct)
         const result = await productsCollection.insertOne(newProduct)
         res.send(result)
     })
+
+    app.put('/products/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedProduct = req.body
+        const product = {
+          $set: {
+            brand: updatedProduct.brand,
+            category: updatedProduct.category,
+            description: updatedProduct.description,
+            photo: updatedProduct.photo,
+            price: updatedProduct.price,
+            rating: updatedProduct.rating,
+            tittle: updatedProduct.tittle
+          }
+        }
+        const result = await productsCollection.updateOne(query, product, options)
+        res.send(result)
+    })
+
+
+    // product cart api
+
+    app.get('/cart/:userId', async(req, res) => {
+
+      const user = req.params.userId
+
+      try {
+        const cursor = cartCollection.find({user: user}); // Filter data based on the brand name
+        const result = await cursor.toArray();
+        res.json(result); // Send the filtered data as a JSON response
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' }); // Handle errors appropriately
+    }
+
+    })
+    
+    app.post('/cart', async(req, res) => {
+      const cartProduct = req.body
+      console.log(cartProduct)
+
+      const result = await cartCollection.insertOne(cartProduct)
+      res.send(result)
+    })
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
